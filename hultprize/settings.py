056c3bfb277b57174af4ea65ffd96d79f1f3b07d
GHOST0O7 <ssr.22u10038@btech.nitdgp.ac.in>
2023-11-13 20:19:04 +0530

    fixed token and animation

--- hultprize/settings.py
+++ hultprize/settings.py
@@ -139,5 +139,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', "mail@example.com")
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', "password")
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', "ssankeerthreddy23@gmail.com")
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', "qibvebcpmruvogwk")
